5cc0772e2b2146dde0b907cd2dc313beba9870d9
hugosenari <hugosenari@gmail.com>
2022-03-12 03:04:46 -0300

    chore: re index gitapi

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim wrapper around the git version control software'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."gitapi-master".url = "path:./master";
+  inputs."gitapi-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."gitapi-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
